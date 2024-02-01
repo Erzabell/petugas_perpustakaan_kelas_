fd8a74ae21d1f5006c1f697bf989db60bc607027
Hendrik Azahra <aremafans863@gmail.com>
2024-02-01 13:48:58 +0700

    rush and push

--- lib/app/modules/peminjaman/views/peminjaman_view.dart
+++ lib/app/modules/peminjaman/views/peminjaman_view.dart
@@ -7,25 +7,25 @@ import '../controllers/peminjaman_controller.dart';
 
 class PeminjamanView extends GetView<PeminjamanController> {
   const PeminjamanView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('PeminjamanView'),
-        centerTitle: true,
-      ),
-      body: controller.obx((state) => ListView.separated(
-        itemCount: state!.length,
-        itemBuilder: (context, index){
-          DataPinjam dataPinjam = state[index];
-          return ListTile(
-            title: Text("${dataPinjam.user?.username}"),
-            subtitle: Text("Judul Buku:${dataPinjam.book?.judul}\n Tanggal Pinjam:${dataPinjam.tanggalPinjam}\n Tanggal Kembali:${dataPinjam.tanggalKembali}\n Status:${dataPinjam.status}"),
-          );
-        },
-        separatorBuilder: (context, index)=> Divider(),
-      ))
-
-    );
+        appBar: AppBar(
+          title: const Text('PeminjamanView'),
+          centerTitle: true,
+        ),
+        body: controller.obx((state) => ListView.separated(
+              itemCount: state!.length,
+              itemBuilder: (context, index) {
+                DataPinjam dataPinjam = state[index];
+                return ListTile(
+                  title: Text("${dataPinjam.user?.username}"),
+                  subtitle: Text(
+                      "Judul Buku:${dataPinjam.book?.judul}\n Tanggal Pinjam:${dataPinjam.tanggalPinjam}\n Tanggal Kembali:${dataPinjam.tanggalKembali}\n Status:${dataPinjam.status}"),
+                );
+              },
+              separatorBuilder: (context, index) => Divider(),
+            )));
   }
 }
